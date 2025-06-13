3214bef25b91d9c17d57f9ccec47f8a3625f7bfa
linomary <linomary@student.21-school.ru>
2025-06-13 13:15:28 +0700

    fix my mistakes

--- src/common/common.c
+++ src/common/common.c
@@ -11,6 +11,26 @@ void s21_decimal_init(s21_decimal *dec) {
   memset(dec, 0, sizeof(s21_decimal));  // set 0
 }
 
+int s21_decimal_get_sign(s21_decimal *dec) {
+  return (dec->bits[3] & 0x80000000) == 0x80000000;
+}
+
+int s21_decimal_get_scale(s21_decimal *dec) {
+  return (dec->bits[3] >> 16) & 0xFF;
+}
+
+int s21_decimal_get_power(s21_decimal *dec) {
+  int i = 95;
+  if (is_s21_decimal_zero(dec)) {
+    i = 0;
+  } else {
+    while (s21_get_bit(dec,i) == 0) {
+      i--;
+    }
+  }
+  return i;
+}
+
 void s21_decimal_set_sign(s21_decimal *dec, int sign) {
   if (sign)
     dec->bits[3] |= 0x80000000;  // 1 – negative
@@ -19,16 +39,8 @@ void s21_decimal_set_sign(s21_decimal *dec, int sign) {
 }
 
 void s21_decimal_set_scale(s21_decimal *dec, int scale) {
-  dec->bits[3] &= 0xFF00FFFF;              // erase sign
-  dec->bits[3] |= ((scale & 0xFF) << 16);  // set sign
-}
-
-int s21_decimal_get_sign(s21_decimal *dec) {
-  return (dec->bits[3] & 0x80000000) == 0x80000000;
-}
-
-int s21_decimal_get_scale(s21_decimal *dec) {
-  return (dec->bits[3] >> 16) & 0xFF;
+  dec->bits[3] &= 0xFF00FFFF;              // erase scale
+  dec->bits[3] |= ((scale & 0xFF) << 16);  // set scale
 }
 
 // -=-=-=- ACCESSORIES -=-=-=-
