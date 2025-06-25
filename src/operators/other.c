2b6fa87e74205b695f5f165d9daef809dcebe564
linomary <linomary@student.21-school.ru>
2025-06-26 00:35:17 +0700

    Other tests

--- src/operators/other.c
+++ src/operators/other.c
@@ -1,5 +1,6 @@
 #include "../common/common.h"
 
+
 int s21_floor(s21_decimal value, s21_decimal *result) {
   int ret_val = s21_truncate(value, result);
   if (ret_val && !s21_is_equal(*result, value) && s21_decimal_get_sign(&value))
@@ -10,7 +11,7 @@ int s21_floor(s21_decimal value, s21_decimal *result) {
 int s21_round(s21_decimal value,
               s21_decimal *result) {
   int ret_val = s21_truncate(value, result);
-  if (ret_val && !s21_is_equal(*result, value)) {
+  if (!ret_val && !s21_is_equal(*result, value)) {
     if (s21_decimal_get_sign(&value))
       ret_val = s21_sub(value, (s21_decimal){{5, 0, 0, 65536}}, &value);
     else
@@ -21,24 +22,25 @@ int s21_round(s21_decimal value,
 }
 
 int s21_truncate(s21_decimal value, s21_decimal *result) {
-  int ret_val = is_s21_decimal_valid(&value) && is_s21_decimal_valid(result);
-  if (ret_val) {
+  int ret_val = 0;
+  if (!ret_val) {
     int scale = s21_decimal_get_scale(&value);
-    for (int i = 0; i < scale; i++) {
+    for (int i = 0; i < scale && !ret_val; i++) {
       s21_decimal_div_by_10(&value);
     }
-    s21_decimal_set_scale(&value, s21_decimal_get_scale(&value) - scale);
+    if(!ret_val)
+      s21_decimal_set_scale(&value, s21_decimal_get_scale(&value) - scale);
     *result = value;
   }
   return ret_val;
 }
 
 int s21_negate(s21_decimal value, s21_decimal *result) {
-  int ret_val = is_s21_decimal_valid(&value) && is_s21_decimal_valid(result);
-  if (ret_val) {
+  int ret_val = 0;
+  if (!ret_val) {
     *result = value;
-    s21_decimal_set_sign(result, !s21_decimal_get_sign(result));
+    if(!s21_is_equal(*result, (s21_decimal){{0, 0, 0, 0}}))
+      s21_decimal_set_sign(result, !s21_decimal_get_sign(result));
   }
-  ret_val = is_s21_decimal_valid(result);
   return ret_val;
 }
